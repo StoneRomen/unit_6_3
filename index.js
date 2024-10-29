b3fff60e36c6ad2aa4a7f3c557940b0d25cda88f
StoneRomen <184683610+StoneRomen@users.noreply.github.com>
2024-10-29 20:48:33 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+
+// Реалізуйте функцію removeElement(array, item), щоб видалити елемент item з масиву array.
+
+
+function removeElement(array, item) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === item) {
+            array.splice(i, 1);
+            break;
+        }
+    }
+    return array;
+}
+const array = [1, 3, 4, 6, 2, 5, 7];
+removeElement(array, 4);
+console.log(array);
